bead6bcc942ebeabccbe4a47d23121af1b183414
Araphlen <64477685+Araphlen@users.noreply.github.com>
2023-11-28 14:57:57 +0100

    ajout de la perte d'énergie sur le trajet

--- methods/simulated-annealing/main.py
+++ methods/simulated-annealing/main.py
@@ -13,11 +13,22 @@ MAX_DISTANCE=5000  #en mètres ( le drone peut parcourir 14 km avec une batterie
 
 
 
-distances = np.zeros((NOMBRE_DE_COLIS))
-#coordonnées des colis (x,y)
+#coordonnées des colis (x,y) et poids (kg)
+details_colis = np.zeros((NOMBRE_DE_COLIS, 3))
+batterie_drones = np.zeros((NOMBRE_DE_DRONES, 1))
 
-coord_destination_colis = np.zeros((NOMBRE_DE_COLIS, 2))
+
 #TODO : afficher les coordonnées des colis dans un graphique (x,y)
+
+#initialisation des coordonnées des colis aléatoirement dans un cercle de rayon MAX_DISTANCE
+def init_parametres_colis():
+    for i in range(NOMBRE_DE_COLIS):
+        #coordonnées du colis
+        details_colis[i][0]=np.random.uniform(-MAX_DISTANCE,MAX_DISTANCE)
+        details_colis[i][1]=np.random.uniform(-MAX_DISTANCE,MAX_DISTANCE)
+        #poids du colis
+        details_colis[i][2]=np.random.rand(0,MAX_POIDS_COLIS)
+
 
 #calcul de la perte de batterie en fonction de la distance et du poids du colis (en %)
 def calc_pourcentage_perte_batterie(distance,poids_colis):
@@ -25,11 +36,8 @@ def calc_pourcentage_perte_batterie(distance,poids_colis):
     perte_energie =distance*100/d_max_charge
     return perte_energie
 
-#initialisation des coordonnées des colis aléatoirement dans un cercle de rayon MAX_DISTANCE
-for i in range(NOMBRE_DE_COLIS):
-    coord_destination_colis[i][0]=np.random.uniform(-MAX_DISTANCE,MAX_DISTANCE)
-    coord_destination_colis[i][1]=np.random.uniform(-MAX_DISTANCE,MAX_DISTANCE)
-
+def perte_energie_decolage(poids_colis):
+    return 
 
 max_iterations=100
 T=1000
@@ -38,27 +46,27 @@ facteur=0.90
 #solution initiale aléatoire
 #le drone (ID) va livrer le colis (indice)
 solution_initiale=np.random.randint(1,NOMBRE_DE_DRONES+1,NOMBRE_DE_COLIS)
-print(solution_initiale)
 
 #calcul de la distance allée-retour vers la destination du colis a partir du centre de tri
 def cal_distance(coord_colis):
     return np.sqrt(coord_colis[0]**2+coord_colis[1]**2)*2
 
 def calc_energy_needed(distance,poids_colis):
-    return energy_decollage*2 +energy_vol*distance *2 
+    pertealler = calc_pourcentage_perte_batterie(distance,poids_colis)
+    perte_retour = calc_pourcentage_perte_batterie(distance,0) 
 
 
-#calcul du cout de la solution initiale
-def calc_cout(solution_initiale,coord_destination_colis,VITESSE_DRONE,NOMBRE_DE_COLIS):
+def calc_temps_livraison(id_drone,coord_next_colis):
+    dist_dep_house=cal_distance(coord_next_colis)
+
+
+
+
+#calcul du cout de la solution
+def calc_cout(solution,coord_destination_colis,VITESSE_DRONE,NOMBRE_DE_COLIS):
     drones_total_time=np.zeros((NOMBRE_DE_DRONES))
     for i in range(NOMBRE_DE_COLIS):
-        num_drone=solution_initiale[i]
+        num_drone=solution[i]
         dest_colis=coord_destination_colis[i]
         drones_total_time[num_drone-1]+=cal_distance(dest_colis)/VITESSE_DRONE+TEMPS_DECOLAGE*2
 
-
-
-    
-    
-    
-#     return cout
