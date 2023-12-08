3c3ead27c83cd76b70537eba763181a1b15e893a
DGoblot <90686224+DGoblot@users.noreply.github.com>
2023-12-08 01:25:11 +0100

    Update run_solver.cmd

--- methods/glpk-solver/run_solver.cmd
+++ methods/glpk-solver/run_solver.cmd
@@ -1,2 +1,2 @@
-glpsol --model solver_drone.mod --data solver_drone.dat --output solver_drone_solution.log --tmlim 10 --log solver_drone_debug.log
+glpsol --model solver_drone.mod --data solver_drone.dat --output solver_drone_solution.log --log solver_drone_cmd_output.log --tmlim 10
 pause
