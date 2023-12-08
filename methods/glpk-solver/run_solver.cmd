cb85e51078602f912d54e448dc0c0b072531b0c0
SkyMound <crafteurkiller@gmail.com>
2023-12-08 13:30:08 +0100

    Removed useless constraints

--- methods/glpk-solver/run_solver.cmd
+++ methods/glpk-solver/run_solver.cmd
@@ -1,2 +1,2 @@
-glpsol --model solver_drone.mod --data solver_drone.dat --output solver_drone_solution.log --log solver_drone_cmd_output.log --tmlim 10
+glpsol --model solver_drone.mod --data solver_drone.dat --output solution.log --log cmd_output.log --tmlim 10
 pause
