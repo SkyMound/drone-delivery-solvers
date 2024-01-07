1c2552a8d4cabc0ee127520e7eda21ae79e07f24
SkyMound <crafteurkiller@gmail.com>
2024-01-07 22:11:15 +0100

    Exemple of import

--- methods/simulated-annealing/test.py
+++ methods/simulated-annealing/test.py
@@ -1,7 +1,21 @@
 from DeliveryProblem import DeliveryProblem 
-
+from SimulatedAnnealing import SimulatedAnnealing
+from Drone import Drone
+from Order import Order
+from matplotlib import pyplot as plt
 def test() :
-    dp = DeliveryProblem()
-
+    a = DeliveryProblem.import_for_visualisation("test.csv")
+    print(a)
+    # dp = DeliveryProblem(5, 40,filepath="dataDelivery100.csv")
+    # sa = SimulatedAnnealing(1000, 0.95)
+    
+    # best_cost, best_solution, costs = sa.solve(dp, 100, 20)
+    # print(best_cost)
+    # dp.print_solution(best_solution)
+    
+    # dp.export_for_visualisation(best_solution, "test.csv")
+    # plot costs
+    # plt.plot(costs)
+    # plt.show()
 
 test()
