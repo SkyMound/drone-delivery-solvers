c646579be2b0a40d2ace32b3178f84f2ec072046
SkyMound <crafteurkiller@gmail.com>
2024-01-08 01:58:59 +0100

    Add random search and improved algo

--- methods/simulated-annealing/SimulatedAnnealing.py
+++ methods/simulated-annealing/SimulatedAnnealing.py
@@ -12,6 +12,7 @@ class SimulatedAnnealing :
         costs= np.zeros(iterations)
         
         current_solution = problem.generate_solution()
+        potential_solution = copy.deepcopy(current_solution)
         best_solution = copy.deepcopy(current_solution)
         costs[0] = best_cost = current_cost = problem.cost(current_solution)
                 
@@ -20,33 +21,46 @@ class SimulatedAnnealing :
             #print('la ',i,'è_me solution = ',solution,' donne le temps maximum de livraison = ',cout0,' la température actuelle =',T)
             T=T*self.factor
             
+            # set potential cost to infinity float
+            potential_cost = float('inf')
+            
             # Deep search (optimizing current solution)
             new_solution = problem.optimize(current_solution)
             new_cost = problem.cost(new_solution)
-            if new_cost < current_cost:
-                current_cost = new_cost
-                current_solution = copy.deepcopy(new_solution)
-                if new_cost < best_cost:
-                    best_cost = new_cost
-                    best_solution = copy.deepcopy(new_solution)
+            if new_cost < potential_cost and new_cost != current_cost:
+                potential_cost = new_cost
+                potential_solution = copy.deepcopy(new_solution)
             
             # Local search (explore neighbor solutions)
-            for j in range(batch_size):
+            for _ in range(batch_size):
                 new_solution = problem.get_neighbor(current_solution)
                 new_cost = problem.cost(new_solution)
-                if new_cost < current_cost:
-                    current_cost = new_cost
-                    current_solution = copy.deepcopy(new_solution)
-                    if new_cost < best_cost:
-                        best_cost = new_cost
-                        best_solution = copy.deepcopy(new_solution)
-                elif new_cost != current_cost :
-                    x=np.random.uniform()
-                    if x<np.exp((current_cost-new_cost)/T):
-                        print(np.exp((current_cost-new_cost)/T),T,current_cost,new_cost)
-                        current_cost=new_cost
-                        current_solution=copy.deepcopy(new_solution)
-                        
-                costs[i] = current_cost
-                
+                if new_cost < potential_cost and new_cost != current_cost:
+                    potential_cost = new_cost
+                    potential_solution = copy.deepcopy(new_solution)
+            
+            # Random search (generate new solution)
+            new_solution = problem.generate_solution()
+            new_cost = problem.cost(new_solution)
+            if new_cost < potential_cost and new_cost != current_cost:
+                potential_cost = new_cost
+                potential_solution = copy.deepcopy(new_solution)
+            
+            # Update current solution if better or according to tempature
+            if potential_cost < current_cost:
+                current_cost = potential_cost
+                current_solution = copy.deepcopy(potential_solution)
+                if potential_cost < best_cost:
+                    best_cost = potential_cost
+                    best_solution = copy.deepcopy(potential_solution)
+            elif potential_cost > current_cost :
+                x=np.random.uniform()
+                if x<np.exp((current_cost-potential_cost)/T):
+                    print("yess")
+                    
+                    current_cost=potential_cost
+                    current_solution=copy.deepcopy(potential_solution)
+            
+            costs[i] = current_cost
+            
         return best_cost, best_solution,costs
